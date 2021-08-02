803d736a2b9a45ec9dc5a7f1bac292f8cf3db698
jpromanonet <programming@jpromano.net>
2021-08-02 09:49:24 -0300

    Deploying node to AWS, Azure an Gcloud

--- nodeDocker/index.js
+++ nodeDocker/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express();
 
 app.get('/', function(req,res){
-    res.send("Hello World!");
+    res.send("Welcome! to my first docker container in Node deployed in Azure, AWS and Google Cloud =)");
 });
 
 app.listen(8000);
